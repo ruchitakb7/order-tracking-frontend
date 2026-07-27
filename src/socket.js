6d4854f84b82553fa295a61b4bcb42a70af53c52
ruchitakb7 <ruchitakb7@gmail.com>
2026-07-27 21:27:52 +0530

    hardcoded socket url

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,13 @@
-console.log("socket.js loaded");
 
 import { io } from "socket.io-client";
+console.log("socket.js loaded");
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+const SOCKET_URL = "https://order-tracking-backend-uaj0.onrender.com";
 
-// console.log(import.meta.env.VITE_SOCKET_URL);
+
+// const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+
+//  console.log(import.meta.env.VITE_SOCKET_URL);
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
